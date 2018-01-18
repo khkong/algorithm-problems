de8c12b91a19da55f1670838735465703e733624
khkong <khkong0928@naver.com>
2018-01-18 22:44:44 +0900

    solve ABBA

--- src/single_250/AB.java
+++ src/single_250/AB.java
@@ -1,13 +1,5 @@
 package single_250;
 
-/*Problem Statement
-        You are given two s: N and K. Lun the dog is interested in strings that satisfy the following conditions:
-
-        The string has exactly N characters, each of which is either 'A' or 'B'.
-        The string s has exactly K pairs (i, j) (0 <= i < j <= N-1) such that s[i] = 'A' and s[j] = 'B'.
-
-        If there exists a string that satisfies the conditions, find and return any such string. Otherwise, return an empty string.*/
-
 public class AB {
     public String createString(int n, int k) {
         StringBuffer sb = new StringBuffer();
---
+++ src/single_500/ABBA.java
@@ -0,0 +1,69 @@
+package single_500;
+
+public class ABBA {
+
+    private StringBuilder mStrBuilder;
+
+    public ABBA() {
+        mStrBuilder = new StringBuilder();
+    }
+
+    public String canObtain(String initial, String target) {
+        return recursion(initial, target) ? "Possible" : "Impossible";
+    }
+
+    private boolean recursion2(String initial, String target) {
+        if(initial.length() == target.length())
+            return initial.equals(target);
+
+        //rule1
+        String rule1 = initial + 'A';
+        mStrBuilder.setLength(0);
+        mStrBuilder.append(rule1);
+        String revRule1 = mStrBuilder.reverse().toString();
+
+        if (target.contains(rule1) || target.contains(revRule1)) {
+            return recursion(rule1, target);
+        }
+
+        //rule2
+        mStrBuilder.setLength(0);
+        mStrBuilder.append(initial);
+        String rule2 = mStrBuilder.reverse().toString() + 'B';
+        mStrBuilder.setLength(0);
+        mStrBuilder.append(rule2);
+        String revRule2 = mStrBuilder.reverse().toString();
+
+        if (target.contains(rule2) || target.contains(revRule2)) {
+            return recursion(rule2, target);
+        }
+
+        return false;
+    }
+
+    private boolean recursion(String initial, String target) {
+        if(initial.length() == target.length())
+            return initial.equals(target);
+
+        int len = target.length();
+        char last = target.charAt(len-1);
+        String newTarget;
+        if(last == 'A'){
+            newTarget=  target.substring(0,len-1);
+        }else{
+            mStrBuilder.setLength(0);
+            mStrBuilder.append(target);
+            mStrBuilder.setLength(len - 1);
+            newTarget=  mStrBuilder.reverse().toString();
+        }
+        return recursion2(initial, newTarget);
+    }
+
+    public static void main(String[] args) {
+        ABBA abba = new ABBA();
+//        System.out.println(abba.canObtain("AB","ABB"));
+//        System.out.println(abba.canObtain("BBAB","ABABABABB"));
+        System.out.println(abba.canObtain("BBBBABABBBBBBA", "BBBBABABBABBBBBBABABBBBBBBBABAABBBAA"));
+//        System.out.println(abba.canObtain("A","BB"));
+    }
+}
